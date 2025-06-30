2a26ae4386b957c0295c5df0939431a07ee49761
werzl <adam@houseofhewitt.co.uk>
2025-06-30 01:32:39 +0100

    run GPT prompt on uploaded image, base64 encoded

--- ocr-shopping-list-vite/src/App.jsx
+++ ocr-shopping-list-vite/src/App.jsx
@@ -1,73 +1,115 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState, useRef } from "react";
 import notepadLogo from "./assets/notepad.png";
 import { recogniseHandwriting } from "./openai/openAiWrapper.js";
+//import { recogniseHandwriting } from "../test/openai/openAiWrapperMock";
 import "./App.css";
 
-// Replace with your image URL
-// const imageUrl = "https://i.imgur.com/wzYpVgz_d.jpeg";
-// await recognizeHandwriting(imageUrl);
-
 function App() {
+  const [base64ImageFileName, setBase64ImageFileName] = useState("");
+  const [base64Image, setBase64Image] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [shoppingList, setShoppinglist] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const shoppingListRef = useRef(null);
+
   async function handleProcess() {
     setIsLoading(true);
 
+    console.log(`base64 image: ${base64Image}`);
     const commaSeparated = await recogniseHandwriting(imageUrl);
     const array = commaSeparated.split(',');
 
     setShoppinglist(array);
     setIsLoading(false);
+
+    shoppingListRef.current.scrollIntoView({ behaviour: "smooth" });
+  }
+
+  function handleImageUpload(e) {
+    const file = e.target.files[0];
+
+    try {
+      if (!file)
+        throw new Error("File was empty");
+
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const base64String = `data:${file.type};base64,${reader.result.split(",")[1]}`;
+        setBase64ImageFileName(file.name);
+        setBase64Image(reader.result);
+        setImageUrl(base64String);
+      };
+
+      reader.readAsDataURL(file);
+    }
+    catch (e) {
+      console.error(e);
+    }
   }
 
   function handleCLear() {
     setImageUrl("");
     setShoppinglist([]);
+    setBase64ImageFileName("");
+    setBase64Image("");
+    setIsLoading(false);
   }
 
   return (
     <>
       <div>
-        <a href="" target="_blank">
+        <a>
           <img src={notepadLogo} className="logo" alt="Notepad logo" />
         </a>
       </div>
-      <h1>HandyList 1.0</h1>
+      <h2>HandyList 1.1.0</h2>
       <div className="card">
-        <input type="text" placeholder="Image URL" value={imageUrl} onChange={(e) => setImageUrl(e.target.value )} />
+        <div className="file-input">
+          <input type="file" id="file" className="file" accept="image/jpeg, image/jpg, image/png, image/webp, image/gif" onChange={handleImageUpload} />
+          <label htmlFor="file">
+            Upload photo
+          </label>
+        </div>
 
-        <br /><br />
-        <button onClick={() => handleProcess()}>
-          Process
+        <button className="button-30" onClick={async () => await handleProcess()} disabled={isLoading} style={{"width": "100px"}}>
+          {!isLoading && "Process" }
+
+          {isLoading && <div className="loader"></div> }
         </button>
 
         <br /><br />
-        <button onClick={() => handleCLear()}>
+        <button className="button-30" onClick={() => handleCLear()} disabled={isLoading}>
           Clear
         </button>
       </div>
 
-      <div style={{textAlign: "center"}}>
-        {!isLoading && <>
-            <a href="" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </>}
+      {base64Image && 
+          <div className="uploaded-image-container">
+            <p>{base64ImageFileName}</p>
+            <img src={base64Image} className="uploaded-image" />
+          </div>
+        }
 
-          {isLoading && <>
-            <a href="" target="_blank">
-              <img src={reactLogo} className="logo logo-spin react" alt="React logo" />
-            </a>
-          </>}
-      </div>
+      <div className="shopping-list" ref={shoppingListRef}>
+        {shoppingList.map(item => (
+          <>
+            <div className="checkbox-wrapper-52" key={item}>
+              <label htmlFor={item} className="item">
+                <input type="checkbox" id={item} className="hidden"/>
 
-      <div style={{textAlign: "left"}}>
-          {shoppingList.map(item => (
-            <p><input style={{width: "100%"}} type="checkbox"  value={item} /> {item}</p>
-          ))}
+                <label htmlFor={item} className="cbx">      
+                  <svg width="14px" height="12px" viewBox="0 0 14 12">
+                    <polyline points="1 7.6 5 11 13 1"></polyline>
+                  </svg>
+                </label>
+
+                <label htmlFor={item} className="cbx-lbl">{item}</label>
+              </label>
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
---
+++ ocr-shopping-list-vite/test/openai/openAiWrapperMock.js
@@ -0,0 +1,5 @@
+
+export function recogniseHandwriting(imageUrl) {
+	console.info(`Received imageUrl: ${imageUrl}`);
+	return "milk,bread,eggs,apples,oranges,chicken,salmon,rice,pasta,cereal";
+}
