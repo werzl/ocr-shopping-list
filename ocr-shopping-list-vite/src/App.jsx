f4d0dae029ded7a9a45d65962fe1b9240c93eb0e
werzl <adam@houseofhewitt.co.uk>
2026-02-21 19:13:56 +0000

    v1.5.0 - install bootstrap react, arrange buttons in app bottom bar layout

--- ocr-shopping-list-vite/src/App.jsx
+++ ocr-shopping-list-vite/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState, useRef, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import { Container, Row, Col, Card, Navbar } from "react-bootstrap";
 
 import notepadLogo from "./assets/notepad.png";
 import placeholder from "./assets/placeholder.png";
 import OpenAiWrapper from "./openai/OpenAiWrapper.js";
+import photoIcon from "./assets/photo-film-solid-full.svg";
+import playIcon from "./assets/play-solid-full.svg";
+import trashIcon from "./assets/trash-solid-full.svg";
+import leftIcon from "./assets/arrow-left-solid-full.svg";
 import "./App.css";
 
 const shoppingListStorageKey = "shoppingList";
 const base64ImageStorageKey = "base64Image";
 const placeholderStorageValue = "placeholder";
 const base64FileNameStorageKey = "base64FileName";
+
+const appVersion = __APP_VERSION__; // Injected at build time from vite.config.js
 
 function App() {
   const [base64ImageFileName, setBase64ImageFileName] = useState(() => {
@@ -35,6 +42,7 @@ function App() {
 
   const shoppingListRef = useRef(null);
   const openAiWrapper = useRef(null);
+  const inputFileButtonRef = useRef(null);
 
   useEffect(() => {
     localStorage.setItem(shoppingListStorageKey, JSON.stringify(shoppingList));
@@ -104,8 +112,11 @@ function App() {
 
   async function handleImageUpload(e) {
     handleClear();
-
+    
     const file = e.target.files[0];
+    
+    // Clear the file input so that the same file can be uploaded again if needed without having to manually clear the input
+    inputFileButtonRef.current.value = null;
 
     try {
       if (!file)
@@ -183,93 +194,113 @@ function App() {
 
   return (
     <>
-      <div>
-        <a>
-          <img src={notepadLogo} className="logo" alt="Notepad logo" />
-        </a>
-      </div>
-      <h2>HandyList {__APP_VERSION__}</h2>
-
-      {!apiKeyConfirmed &&
-        <div className="div">
-          <form className="w-25 mt-3 text-center" >
-            <label htmlFor="apiKey">Enter an API Key</label>
-            <br />
-            <input type="password" id="apiKey" placeholder="API-Key" onChange={e => setApiKey(e.target.value)} style={{ "width": "400px" }} />
-            <br /><br />
-            <button type="button" className="button-30" onClick={() => handleApiKeyConfirmation()}>
-              Submit
-            </button>
-          </form>
-        </div>
-      }
-
-      {apiKeyConfirmed &&
-        <>
-          <div className="card">
-            <button className="button-30 dark" onClick={() => handleBack()} disabled={isLoading}>
-              {"<"} Back
-            </button>
-
-            <br /><br />
-
-            <input type="file" id="upload" accept="image/jpeg, image/jpg, image/png, image/webp, image/gif" onChange={handleImageUpload} style={{display: "none"}} disabled={isLoading}/>
-            <label htmlFor="upload" className="button-30 upload" disabled={isLoading}>
-              Upload Photo
-            </label>
-
-            <br /><br />
-
-            <button className={`button-30 ${isLoading ? "loading" : ""}`} onClick={async () => await processImage()} disabled={isLoading || shoppingList.length > 0 || !imageUrl} style={{ "width": "100px" }}>
-              {!isLoading && "Process"}
-
-
-              {isLoading && <div className="loader"></div>}
-            </button>
-
-            <br /><br />
-
-            <button className="button-30" onClick={() => handleClear()} disabled={isLoading}>
-              Clear
-            </button>
-          </div>
-
-          {base64Image &&
-            <div className="uploaded-image-container">
-              <p>{base64ImageFileName}</p>
-              <img src={base64Image === placeholderStorageValue ? placeholder : base64Image} className="uploaded-image" />
+      <Container>
+        <Row>
+          <Col>
+            <div>
+              <a>
+                <img src={notepadLogo} className="logo" alt="Notepad logo" />
+              </a>
             </div>
-          }
+            <h2>HandyList {appVersion}</h2>
 
-          <div className="shopping-list" ref={shoppingListRef}>
-            {shoppingList.length > 0 &&
+            {!apiKeyConfirmed &&
+              <div className="div">
+                <form className="w-25 mt-3 text-center" >
+                  <label htmlFor="apiKey">Enter an API Key</label>
+                  <br />
+                  <input type="password" id="apiKey" placeholder="API-Key" onChange={e => setApiKey(e.target.value)} style={{ "width": "400px" }} />
+                  <br /><br />
+                  <button type="button" className="button-30" onClick={() => handleApiKeyConfirmation()}>
+                    Submit
+                  </button>
+                </form>
+              </div>
+            }
+
+            {apiKeyConfirmed &&
               <>
-                {shoppingList.map(item => {
-                  let key = Math.random();
+                {base64Image &&
+                  <div className="uploaded-image-container">
+                    <p>{base64ImageFileName}</p>
+                    <img src={base64Image === placeholderStorageValue ? placeholder : base64Image} className="uploaded-image" />
+                  </div>
+                }
 
-                  return (
-                    <div className="checkbox-wrapper-52" key={key}>
-                      <label htmlFor={key} className="item">
-                        <input type="checkbox" id={key} className="hidden" checked={item.checked} onChange={(e) => handleCheck(item.name, e.target.checked)} />
+                <div className="shopping-list" ref={shoppingListRef}>
+                  {shoppingList.length > 0 &&
+                    <>
+                      {shoppingList.map(item => {
+                        let key = Math.random();
 
-                        <label htmlFor={key} className="cbx">
-                          <svg width="14px" height="12px" viewBox="0 0 14 12">
-                            <polyline points="1 7.6 5 11 13 1"></polyline>
-                          </svg>
-                        </label>
+                        return (
+                          <div className="checkbox-wrapper-52" key={key}>
+                            <label htmlFor={key} className="item">
+                              <input type="checkbox" id={key} className="hidden" checked={item.checked} onChange={(e) => handleCheck(item.name, e.target.checked)} />
 
-                        <label htmlFor={key} className="cbx-lbl">{item.name}</label>
-                      </label>
-                    </div>);
-                })}
+                              <label htmlFor={key} className="cbx">
+                                <svg width="14px" height="12px" viewBox="0 0 14 12">
+                                  <polyline points="1 7.6 5 11 13 1"></polyline>
+                                </svg>
+                              </label>
+
+                              <label htmlFor={key} className="cbx-lbl">{item.name}</label>
+                            </label>
+                          </div>);
+                      })}
+                    </>
+                  }
+
+                  {shoppingList.length === 0 && base64Image === "" &&
+                    <Card bg="light" text="dark" className="mt-5 text-start">
+                      <Card.Header className="border-bottom">Upload an image</Card.Header>
+                      <Card.Body>
+                        <Card.Text>
+                          No items found. Upload an image of a handwritten shopping list and click the play button to extract the text.
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  }
+                </div>
               </>
             }
-          </div>
-        </>
+
+            <ToastContainer />
+          </Col>
+        </Row>
+      </Container>
+
+      {apiKeyConfirmed &&
+        <Navbar fixed="bottom" variant="dark" text="light" className="justify-content-center" style={{ paddingBottom: "42.67px", borderTop: "1px solid white", backgroundColor: "#242424" }}>
+          <Row>
+            <Col>
+              <button className="nav-button" onClick={() => handleBack()} disabled={isLoading}>
+                <img src={leftIcon} alt="Back" style={{ width: "35px", height: "35px" }} />
+              </button>
+            </Col>
+
+            <Col className="align-content-center">
+              <input ref={inputFileButtonRef} type="file" id="upload" accept="image/jpeg, image/jpg, image/png, image/webp, image/gif" onChange={handleImageUpload} style={{ display: "none" }} disabled={isLoading} />
+              <label htmlFor="upload" disabled={isLoading}>
+                <img src={photoIcon} alt="Upload" style={{ width: "35px", height: "35px" }} />
+              </label>
+            </Col>
+
+            <Col className="align-content-center">
+              <button className={`nav-button ${isLoading ? "loading" : ""}`} onClick={async () => await processImage()} disabled={isLoading || shoppingList.length > 0 || !imageUrl}>
+                {!isLoading && <img src={playIcon} alt="Process" style={{ width: "35px", height: "35px" }} className={`${base64Image && shoppingList.length === 0 ? "pulse" : ""}`}/>}
+                {isLoading && <div className="loader"></div>}
+              </button>
+            </Col>
+
+            <Col className="align-content-center">
+              <button className="nav-button" onClick={() => handleClear()} disabled={isLoading}>
+                <img src={trashIcon} alt="Clear" style={{ width: "35px", height: "35px" }} />
+              </button>
+            </Col>
+          </Row>
+        </Navbar>
       }
-
-      <ToastContainer />
-
     </>
   );
 }
